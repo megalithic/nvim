b44c894222759b475e98c0417848f1964b6fa229
Seth Messer <seth@megalithic.io>
2024-04-19 11:28:35 -0400

    wip: trying to get my startup mappings to work

--- lua/plugins/extended/telescope.lua
+++ lua/plugins/extended/telescope.lua
@@ -51,6 +51,7 @@ return {
       mega.picker = {
         find_files = {},
         grep = {},
+        startup = {},
       }
 
       local fmt = string.format
@@ -211,7 +212,7 @@ return {
       -- * cwd
       ---@param opts? table
       local find_files = function(opts)
-        opts = opts or {}
+        opts = vim.tbl_deep_extend("force", opts, {})
         local theme = opts["theme"] or "ivy"
         local bufnr = vim.api.nvim_get_current_buf()
         local fn = vim.api.nvim_buf_get_name(bufnr)
@@ -230,7 +231,7 @@ return {
         end
       end
       mega.picker.find_files = find_files
-      mega.picker.startup = function()
+      mega.picker.startup = function(bufnr)
         find_files({
           theme = "dropdown",
           hidden = true,
@@ -245,13 +246,13 @@ return {
             i = {
               ["<cr>"] = stopinsert(function(pb)
                 multi(pb, "edit")
-                vim.api.nvim_buf_delete(1, { force = true })
+                vim.schedule(function() vim.api.nvim_buf_delete(bufnr + 1, { force = true }) end)
               end),
             },
             n = {
               ["<cr>"] = function(pb)
                 multi(pb, "vnew")
-                vim.api.nvim_buf_delete(args.buf + 1, { force = true })
+                vim.schedule(function() vim.api.nvim_buf_delete(bufnr + 1, { force = true }) end)
               end,
             },
           },
@@ -369,33 +370,33 @@ return {
           layout_config = {
             prompt_position = "top",
           },
-          buffer_previewer_maker = new_maker,
-          preview = {
-            mime_hook = function(filepath, bufnr, opts)
-              local is_image = function(fp)
-                local image_extensions = { "png", "jpg" } -- Supported image formats
-                local split_path = vim.split(fp:lower(), ".", { plain = true })
-                local extension = split_path[#split_path]
-                return vim.tbl_contains(image_extensions, extension)
-              end
-
-              if is_image(filepath) then
-                local term = vim.api.nvim_open_term(bufnr, {})
-                local function send_output(_, data, _)
-                  vim.pprint(data)
-                  for _, d in ipairs(data) do
-                    vim.api.nvim_chan_send(term, d .. "\r\n")
-                  end
-                end
-                vim.fn.jobstart({
-                  "catimg",
-                  filepath, -- Terminal image viewer command
-                }, { on_stdout = send_output, stdout_buffered = true, pty = true })
-              else
-                require("telescope.previewers.utils").set_preview_message(bufnr, opts.winid, "Binary cannot be previewed")
-              end
-            end,
-          },
+          -- buffer_previewer_maker = new_maker,
+          -- preview = {
+          --   mime_hook = function(filepath, bufnr, opts)
+          --     local is_image = function(fp)
+          --       local image_extensions = { "png", "jpg" } -- Supported image formats
+          --       local split_path = vim.split(fp:lower(), ".", { plain = true })
+          --       local extension = split_path[#split_path]
+          --       return vim.tbl_contains(image_extensions, extension)
+          --     end
+          --
+          --     if is_image(filepath) then
+          --       local term = vim.api.nvim_open_term(bufnr, {})
+          --       local function send_output(_, data, _)
+          --         vim.pprint(data)
+          --         for _, d in ipairs(data) do
+          --           vim.api.nvim_chan_send(term, d .. "\r\n")
+          --         end
+          --       end
+          --       vim.fn.jobstart({
+          --         "catimg",
+          --         filepath, -- Terminal image viewer command
+          --       }, { on_stdout = send_output, stdout_buffered = true, pty = true })
+          --     else
+          --       require("telescope.previewers.utils").set_preview_message(bufnr, opts.winid, "Binary cannot be previewed")
+          --     end
+          --   end,
+          -- },
           mappings = {
             i = {
               ["<esc>"] = require("telescope.actions").close,
@@ -464,6 +465,7 @@ return {
           "^dotbot",
         },
         pickers = {
+          lsp_definitions = ivy({}),
           highlights = ivy({}),
           find_files = ivy({
             path_display = filename_first,
@@ -551,14 +553,14 @@ return {
             end,
           }),
           lsp_references = ivy({
-            mappings = {
-              i = {
-                ["<cr>"] = stopinsert(function(pb)
-                  -- multi(pb, 'vnew')
-                  actions.select_vertical(pb)
-                end),
-              },
-            },
+            -- mappings = {
+            --   i = {
+            --     ["<cr>"] = stopinsert(function(pb)
+            --       -- multi(pb, 'vnew')
+            --       actions.select_vertical(pb)
+            --     end),
+            --   },
+            -- },
           }),
         },
         extensions = {
